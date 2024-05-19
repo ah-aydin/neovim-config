9fa807781bfd50cacea831973870e635f93916d3
ah-aydin <ahamzaaydin@gmail.com>
2024-05-20 00:01:07 +0200

    window resize keys

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -36,6 +36,11 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- Window resize
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
 
 -- Useless mappings
 vim.keymap.set("n", "[[", "<NOP>")
